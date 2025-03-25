005394b184506ca77bfb774babb8a95f55270b80
Bastien Le Corguille <bastien.bruges@icloud.com>
2025-03-25 17:47:59 +0100

    TP 10 - Register et submit_register finis

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Afficher les erreurs à l'écran
+ini_set('display_errors', 1);
+// Afficher les erreurs et les avertissements
+error_reporting(E_ALL);
+header("Location: register.php");
+?>
---
+++ submit_register.php
@@ -0,0 +1,33 @@
+<?php
+
+$post = $_POST;
+var_dump($post);
+
+if (isset($post['email']) && isset($post['password']) && isset($post['confirm-password']) && $post['password'] === $post['confirm-password']) {
+    echo "ok";
+
+    require_once 'Model/pdo.php'; 
+
+    try {
+        $resultat = $dbPDO->prepare("INSERT INTO `user` (`id`, `email`, `password`) VALUES (NULL, :email, SHA1(:password));");
+        $req = $resultat->execute([
+            'email' => $post['email'],
+            'password' => $post['password']
+        ]);
+
+        if ($req) {
+            echo "Utilisateur ajouté avec succès.";
+            header('Location: dashboard.php');
+            exit();
+        } else {
+            echo "Erreur lors de l'ajout de l'utilisateur.";
+        }
+    } catch (Exception $e) {
+        echo "Erreur : " . $e->getMessage();
+    }
+} else {
+    echo "ko";
+    header('Location: register.php');
+    exit();
+}
+?>
